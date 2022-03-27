51ef670e24672b721e3d400a4c3999681cb496e2
Abinash <abinash261997@gmail.com>
2022-03-27 22:58:33 +0530

    intregate signup api

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/User");
 
 const app = express();
 
-app.get("/", (req, resp) => {
-    resp.send("app is working...");
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    resp.send(result);
 });
 
 app.listen(5001);

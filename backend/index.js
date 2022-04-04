6c4bcc3775a141a637934bedcaab9ca73a9ac134
Abinash <abinash261997@gmail.com>
2022-04-04 23:37:19 +0530

    delete search update operation complete in product section

--- backend/index.js
+++ backend/index.js
@@ -45,12 +45,51 @@ app.post("/add-product", async (req, resp) => {
 
 // list product
 app.get("/products", async (req, resp) => {
-    let product = await Product.find();
-    if (product.length > 0) {
-        resp.send(product);
+    let products = await Product.find();
+    if (products.length > 0) {
+        resp.send(products);
     } else {
         resp.send({ result: "No Product Found" });
     }
+});
+
+// delete product
+app.delete("/product/:id", async (req, resp) => {
+    // resp.send(req.params.id);
+    const result = await Product.deleteOne({ _id: req.params.id });
+    resp.send(result);
+});
+
+//get single product for update
+app.get("/product/:id", async (req, resp) => {
+    let result = await Product.findOne({ _id: req.params.id });
+    // console.log(result);
+
+    // resp.send(result);
+    if (result) {
+        resp.send(result);
+        // console.log(result);
+    } else {
+        resp.send({ result: "No Data Found on this id" });
+        // console.log(result);
+    }
+});
+
+// Update Product
+app.put("/product/:id", async (req, resp) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        { $set: req.body }
+    );
+    resp.send(result);
+});
+
+// Search Product
+app.get("/search/:key", async (req, resp) => {
+    let result = await Product.find({
+        $or: [{ productName: { $regex: req.params.key } }],
+    });
+    resp.send(result);
 });
 
 app.listen(5001);
